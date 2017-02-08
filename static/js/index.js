55b9986c1870a08650eaa2c2204b175ac84d7164
Vit Listik <vit.listik@firma.seznam.cz>
2017-02-08 16:33:17 +0100

    fix array

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 var show_more = function (event) {
     event.preventDefault();
-    var elems = event.target.parentNode.parentNode.querySelectorAll("li.hidden");
+    var elems = Array.from(event.target.parentNode.parentNode.querySelectorAll("li.hidden"));
     elems.forEach(function (entry, i) {
         if (i > 10) {
             return;
