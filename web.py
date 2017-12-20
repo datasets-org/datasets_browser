9ebcb8001200d64416acf6b938b48c0121e03e47
Vit Listik <tivvit@seznam.cz>
2017-12-21 00:07:21 +0100

    fixed context

--- web.py
+++ web.py
@@ -110,8 +110,8 @@ if __name__ == "__main__":
     server_conf_order = tuple(server_conf_order) if server_conf_order else None
     datasets_conf_order = tuple(datasets_conf_order) if datasets_conf_order \
         else None
-
-    conf = get_conf(conf_order=server_conf_order)
-    get_ds(conf_order=datasets_conf_order)
+    with app.app_context():
+        conf = get_conf(conf_order=server_conf_order)
+        get_ds(conf_order=datasets_conf_order)
     app.run(host=conf.host,
             port=conf.port)
