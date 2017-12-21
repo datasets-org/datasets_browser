fb256925a24ab7e5fb3b873b77840b746106ccec
Vit Listik <tivvit@seznam.cz>
2017-12-21 16:44:45 +0100

    finished mock

--- mock/mock.py
+++ mock/mock.py
@@ -22,5 +22,33 @@ def main():
     )
 
 
+@app.route("/detail/<id>")
+def detail(id):
+    return json.dumps(
+        {
+            "id": "5a8a97bd-2cbb-4069-afe2-7101450c99b4",
+            "tags": ["eml", "spam"],
+            "paths": ["path/ds"],
+            "links": ["pth/aaa"],
+            "markdowns": ["pth/readme.md"],
+            "data": ["data"],
+            "url": "https://inclass.kaggle.com/c/adcg-ss14-challenge-02-spam"
+                   "-mails-detection/data",
+            "maintainer": "tivvit@example.com",
+            "name": "Kaggle - spam mail ADCG_SS14",
+            "characteristics": [
+                ["data", [["extensions", {"eml": 4327, "ico": 500}],
+                          ["files_cnt", 4334],
+                          ["files_size", "34M"]]]
+            ],
+            "type": "fs",
+            "changelog": [
+                [["markdowns", [], ["pth/readme.md"], 1483053904.5619552135]],
+                [["markdowns", [], ["path/readme.md"], 1483051504.5655066967]]
+            ]
+        }
+    )
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
